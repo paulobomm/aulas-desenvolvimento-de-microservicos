ada81d9e0ef3abdf25993ac65ab52b60a940dac1
Paulo Roberto Bomm <paulobomm@gmail.com>
2026-03-25 21:27:03 -0300

    deletandoTudo

--- src/shared/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { DrizzleService } from "./infra/database/drizzle.service";
-
-@Module({
-  providers: [DrizzleService],
-  exports: [DrizzleService],
-})
-export class SharedModule {}
