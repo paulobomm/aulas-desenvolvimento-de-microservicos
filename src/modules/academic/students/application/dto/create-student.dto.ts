83f2ead33243e5162bcd5811480439bd349b7155
Felipe-Roos <feliperoos941@gmail.com>
2026-04-01 21:45:01 -0300

    Adicionando validação

---
+++ src/modules/academic/students/application/dto/create-student.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateStudentDto {
+    @ApiProperty( {example: "John Doe"} )
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @ApiProperty({ example: "joao@escola.com" })
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @ApiProperty({ example: "123.456.789-00" })
+    @IsString()
+    @IsNotEmpty()
+    document: string;
+
+    @ApiProperty({ example: "2024001" })
+    @IsString()
+    @IsNotEmpty()
+    registration: string;
+}
